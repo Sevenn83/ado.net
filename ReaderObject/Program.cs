0817164b1f62345959f33e581917d8a2884b77a2
Sevenn <thomas.s833@gmail.com>
2016-09-29 10:00:01 +0200

    Connection à la db ok

--- ReaderObject/Program.cs
+++ ReaderObject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Configuration;
 
 namespace ReaderObject
 {
@@ -7,10 +9,27 @@ namespace ReaderObject
     {
         private static void Main()
         {
+            try
+            {
+                var connection = new OracleConnection
+                {
+                    ConnectionString = ConfigurationManager.ConnectionStrings["oracledb"].ConnectionString
+                };
+                connection.Open();
+                Console.WriteLine("Connection établie");
+                connection.Close();
+                connection.Dispose();
+                Console.WriteLine("Appuyez sur une touche pour continuer ...");
+                Console.ReadKey();
 
-            var Employes = new Collection<Employe>();
-
-
+                var Employes = new Collection<Employe>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Appuyez sur une touche pour continuer ...");
+                Console.ReadKey();
+            }
         }
     }
 }
