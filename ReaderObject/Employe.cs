c16bb34b9d931b4281143868abb19deb153993a2
Sevenn <thomas.s833@gmail.com>
2016-09-29 09:07:42 +0200

    démarrage projet

---
+++ ReaderObject/Employe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReaderObject
+{
+    internal class Employe
+    {
+        public short NumEmp { get; set; }
+        public string NomEmp { get; set; }
+        public string PrenomEmp { get; set; }
+        public string Poste { get; set; }
+        public float Salaire { get; set; }
+        public float? Prime { get; set; }
+        public string CodeProjet { get; set; }
+        public short? Superieur { get; set; }
+
+        public override string ToString()
+        {
+            return NumEmp + " " + NomEmp + " " + Poste + " " + Salaire + " " + Prime + " " + CodeProjet + " " +
+                   Superieur;
+        }
+    }
+}
---
+++ ReaderObject/Program.cs
@@ -0,0 +1,11 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace ReaderObject
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+        }
+    }
+}
