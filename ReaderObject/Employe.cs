04be873e9e5300b279139dff2f3a9362cb260c67
Sevenn <thomas.s833@gmail.com>
2016-09-29 10:46:21 +0200

    ex terminé

--- ReaderObject/Employe.cs
+++ ReaderObject/Employe.cs
@@ -13,6 +13,18 @@ namespace ReaderObject
         public string CodeProjet { get; set; }
         public short? Superieur { get; set; }
 
+        public Employe(short numEmp, string nomEmp, string prenomEmp, string poste, float salaire, float? prime, string codeProjet, short? superieur)
+        {
+            NumEmp = numEmp;
+            NomEmp = nomEmp;
+            PrenomEmp = prenomEmp;
+            Poste = poste;
+            Salaire = salaire;
+            Prime = prime;
+            CodeProjet = codeProjet;
+            Superieur = superieur;
+        }
+
         public override string ToString()
         {
             return NumEmp + " " + NomEmp + " " + Poste + " " + Salaire + " " + Prime + " " + CodeProjet + " " +
--- ReaderObject/Program.cs
+++ ReaderObject/Program.cs
@@ -2,7 +2,6 @@
 using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Configuration;
-using System.Data.SqlClient;
 
 namespace ReaderObject
 {
@@ -12,6 +11,7 @@ namespace ReaderObject
         {
             try
             {
+                // Configuration de connexion
                 var connection = new OracleConnection
                 {
                     ConnectionString = ConfigurationManager.ConnectionStrings["oracledb"].ConnectionString
@@ -19,24 +19,42 @@ namespace ReaderObject
 
                 connection.Open();
 
+                // Commande sql
                 var command = new OracleCommand("SELECT * FROM EMPLOYE", connection);
                 var reader = command.ExecuteReader();
 
+                // Création de la collexion
+                var Employes = new Collection<Employe>();
+
+                // Remplisage de la collection
                 if (reader.HasRows)
                 {
                     while (reader.Read())
                     {
-                        Console.WriteLine("{0}\t{1}", reader.GetInt32(0), reader.GetString(1));
+                        Employes.Add(new Employe(
+                            reader.GetInt16(0),
+                            reader.GetString(1),
+                            reader.GetString(2),
+                            reader.GetString(3),
+                            reader.GetFloat(4),
+                            !reader.IsDBNull(5) ? reader.GetFloat(5) : float.Parse("0"),
+                            !reader.IsDBNull(6) ? reader.GetString(6) : "",
+                            !reader.IsDBNull(7) ? reader.GetInt16(7) : Convert.ToInt16(0)
+                            ));
                     }
                 } else 
                     Console.WriteLine("no rows found.");
 
                 connection.Close();
                 connection.Dispose();
+
+                foreach (var employe in Employes)
+                {
+                    Console.WriteLine(employe.ToString());
+                }
+
                 Console.WriteLine("Appuyez sur une touche pour continuer ...");
                 Console.ReadKey();
-
-                var Employes = new Collection<Employe>();
             }
             catch (Exception e)
             {
